035475fba199a563796af0303806e1162e1ff451
Gabriel Jaubert <alu0101240374@ull.edu.es>
2021-04-12 15:47:33 +0100

    inventado

--- src/greedy.cpp
+++ src/greedy.cpp
@@ -8,28 +8,22 @@ std::vector<Machine> Greedy::computePmspSolution(Pmsp pmspObject){
     pmspObject.getTasks().at(minTask).setTctTime(pmspObject.getSetupTime().at(0).at(minTask + 1), 0);
     pmspObject.getS().at(i).getTasks().push_back(pmspObject.getTasks().at(minTask)); //pushear la minima tarea a la i maquina
     pmspObject.getS().at(i).updateTct(pmspObject.getTasks().at(minTask).getTaskTct()); //actualizar TCT de la maquina
-    // std::cout << "id: " << pmspObject.getTasks().at(minTask).getId() << " ";
+    std::cout << "id: " << pmspObject.getTasks().at(minTask).getId() << " ";
     // std::cout << "pt: " << pmspObject.getTasks().at(minTask).getProcessTime() << " ";
-    // std::cout << pmspObject.getS().at(i).getTct() << std::endl;
+    std::cout << pmspObject.getS().at(i).getTct() << std::endl;
   }
   int i = 0;
   int setupTime = 0;
   while (!pmspObject.allVisited()) {
-  // while (i < 3) {
     minMachine = getMinMachine(pmspObject.getS());
     lastItem = pmspObject.getS().at(minMachine).getTasks().size() - 1;
-    //std::cout << pmspObject.getS().at(minMachine).getTasks().size() - 1;
     minTask = getMinTask(pmspObject.getTasks(), pmspObject.getSetupTime(), pmspObject.getS().at(minMachine).getTasks().at(lastItem).getId());
     setupTime = pmspObject.getSetupTime().at(pmspObject.getS().at(minMachine).getTasks().at(lastItem).getId()).at(minTask + 1);
     pmspObject.getTasks().at(minTask).setTctTime(setupTime, pmspObject.getS().at(minMachine).getTasks().at(lastItem).getTaskTct());
     pmspObject.getS().at(minMachine).getTasks().push_back(pmspObject.getTasks().at(minTask));
-    //std::cout << "id " << pmspObject.getS().at(minMachine).getTasks().at(lastItem + 1).getId() << " ";
-    // std::cout << "m: " << minMachine << " ";
-    // std::cout << "mtct: " << pmspObject.getS().at(minMachine).getTct() << " "; // .getTasks().at(minTask - 1).getTaskTct();
-    // std::cout << "taskTct: " << pmspObject.getTasks().at(minTask).getTaskTct();
+    std::cout << "id " << pmspObject.getS().at(minMachine).getTasks().at(lastItem + 1).getId() << " ";
+    std::cout << "mtct: " << pmspObject.getS().at(minMachine).getTct() << " \n"; // .getTasks().at(minTask - 1).getTaskTct();
   }
-  // std::cout << "\n"<<pmspObject.getS().at(0).getTct() << "\n";
-  // std::cout << pmspObject.getS().at(1).getTct() << "\n";
   return pmspObject.getS();
 }
 
@@ -48,7 +42,6 @@ int Greedy::getMinTask(std::vector<Task>& t, table setup, int originTask) {
   t[index].setVisited();
   return index;
 }
-
 
 int Greedy::getMinMachine(std::vector<Machine> m) {
   int min = 9999;
